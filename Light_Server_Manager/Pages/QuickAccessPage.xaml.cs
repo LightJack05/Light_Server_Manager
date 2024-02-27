c0341fa27641a9544e9ccbfcb5557e2463eafab6
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-02-27 17:14:57 +0100

    Fixed rebuild problem

--- Light_Server_Manager/Pages/QuickAccessPage.xaml.cs
+++ Light_Server_Manager/Pages/QuickAccessPage.xaml.cs
@@ -1,13 +1,22 @@
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
+using System.Collections.ObjectModel;
+
 namespace Light_Server_Manager.Pages;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
 public sealed partial class QuickAccessPage : Page
 {
+
+    private ObservableCollection<Object> _displayedObjects = new();
     public QuickAccessPage()
     {
         this.InitializeComponent();
+    }
+
+    private void asbMainSearch_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        
     }
 }
---
+++ Light_Server_Manager/Settings/ServerManagerSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light_Server_Manager.Settings;
+internal class ServerManagerSettings
+{
+    //TODO: Check if default false is a problem!
+    public bool SearchOnEnterOnly { get; set; } = false;
+}
